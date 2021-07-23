59710fdf8822845f4bea9ceb0691a70f6adc8c05
binghe819 <binghe819@naver.com>
2021-07-23 17:01:34 +0900

    chore: 부트 초기 세팅

---
+++ src/main/java/com/binghe/springbootlearningsandbox/SpringBootLearningSandboxApplication.java
@@ -0,0 +1,12 @@
+package com.binghe.springbootlearningsandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLearningSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLearningSandboxApplication.class, args);
+    }
+}
---
+++ src/test/java/com/binghe/springbootlearningsandbox/SpringBootLearningSandboxApplicationTests.java
@@ -0,0 +1,12 @@
+package com.binghe.springbootlearningsandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLearningSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
